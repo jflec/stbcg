2067552bd161d0b1764f82a391068c5e78e56755
joseph <josephflec@gmail.com>
2024-09-19 22:39:05 -0500

    initial commit

---
+++ src/client/java/net/joe/stbcg/SmallToBigChadGuysClient.java
@@ -0,0 +1,10 @@
+package net.joe.stbcg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmallToBigChadGuysClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
